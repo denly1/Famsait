1778b9ef331ed558007e4e40428168872fd3b358
denly1 <isip_a.d.skuratov@mpt.ru>
2026-02-20 00:36:47 +0300

    Подготовка к деплою на production сервер

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [{
+    name: 'family-events',
+    script: 'npm',
+    args: 'start',
+    cwd: '/var/www/family-events',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    }
+  }]
+}
